2609bbb357b054a9100fe1a36a99106202f1785b
SLIIV <YTANTEI@yandex.ru>
2020-02-04 10:31:55 +0800

    First commit

---
+++ Assets/Spider/Scriprs/SpiderController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    public GameObject spider;
+    public float speed;
+    Vector3 direction;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+
+        float forward = Input.GetAxis("Vertical");
+        float right = Input.GetAxis("Horizontal");
+        direction = new Vector3(forward * speed, 0, -right * speed) * Time.deltaTime;
+        animator.SetFloat("Forward", forward);
+        animator.SetFloat("Right", right);
+        
+
+        //transform.position += direction;
+        
+    }
+}
