4b7175847e1dac060bfd24c0f4fe2186ef710f97
SLIIV <YTANTEI@yandex.ru>
2020-02-08 09:33:00 +0800

    Added animations, playerController

--- Assets/Spider/Scriprs/SpiderController.cs
+++ Assets/Spider/Scriprs/SpiderController.cs
@@ -6,26 +6,95 @@ public class SpiderController : MonoBehaviour
 {
     public GameObject spider;
     public float speed;
+    public float jumpSpeed;
     Vector3 direction;
     private Animator animator;
+    Quaternion rotation;
+    private Rigidbody spiderRb;
+    public bool isGround;
+    public float toGround;
+    public bool isDead;
+    private bool isMaxDeathSpeed;
+
     void Start()
     {
         animator = GetComponent<Animator>();
+        spiderRb = spider.GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
-        
+        if (!isDead)
+        {
+            float forward = Input.GetAxis("Vertical");
+            float right = Input.GetAxis("Horizontal");
+            direction = new Vector3(transform.forward.x * speed * -forward, 0, transform.forward.z * speed * -forward) * Time.deltaTime;
+            rotation = Quaternion.Euler(new Vector3(0, right, 0));
+            if (isGround)
+            {
+                animator.SetFloat("Forward", forward);
+                animator.SetFloat("Right", right);
+            }
+            if (!isGround)
+            {
+                animator.SetFloat("Forward", 0);
+                animator.SetFloat("Right", 0);
+            }
+            transform.position += direction;
+            transform.Rotate(rotation.eulerAngles);
 
-        float forward = Input.GetAxis("Vertical");
-        float right = Input.GetAxis("Horizontal");
-        direction = new Vector3(forward * speed, 0, -right * speed) * Time.deltaTime;
-        animator.SetFloat("Forward", forward);
-        animator.SetFloat("Right", right);
-        
 
-        //transform.position += direction;
-        
+
+            animator.SetFloat("vSpeed", spiderRb.velocity.y);
+
+
+
+            Jump();
+            if (Input.GetMouseButtonDown(0) && isGround)
+                animator.SetTrigger("Attack");
+            if (Input.GetMouseButtonDown(2) && isGround)
+            {
+                animator.SetTrigger("WebAttack");
+                spiderRb.AddForce(0, jumpSpeed * Time.deltaTime, 0, ForceMode.Impulse);
+                spiderRb.velocity = Vector3.zero;
+            }
+            if (spiderRb.velocity.y < -12)
+            {
+                isMaxDeathSpeed = true;
+            }
+        }
+        if (isMaxDeathSpeed && isGround)
+        {
+            isDead = true;
+            animator.enabled = false;
+            animator.SetFloat("vSpeed", 0);
+            animator.SetBool("isGround", false);
+            animator.SetTrigger("isDead");
+            animator.enabled = true;
+            
+        }
+    }
+
+
+    private void Jump()
+    {
+        Ray ray = new Ray(spider.transform.position, transform.TransformDirection(Vector3.down));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, toGround))
+        {
+            isGround = true;
+            animator.SetBool("isGround", true);
+        }
+        else
+        {
+            isGround = false;
+            animator.SetBool("isGround", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            spiderRb.velocity = new Vector3(0, 0, 0);
+            spiderRb.AddForce(0, jumpSpeed * Time.deltaTime, 0, ForceMode.Impulse);
+        }
     }
 }
