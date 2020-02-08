57b48d753db2c58e068795fcf1bfde9cfd5325da
SLIIV <YTANTEI@yandex.ru>
2020-02-08 09:39:39 +0800

    Added animations, playerController

---
+++ Assets/GUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class GUIController : MonoBehaviour
+{
+    public SpiderController spider;
+    public TextMesh gameOverText;
+
+
+    private void Start()
+    {
+        if (gameOverText == null)
+        {
+             gameOverText = GetComponentInChildren<TextMesh>();
+        }
+    }
+
+    private void Update()
+    {
+        onFall();
+    }
+    void onFall()
+    {
+       if(spider.isDead)
+        {
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+}
